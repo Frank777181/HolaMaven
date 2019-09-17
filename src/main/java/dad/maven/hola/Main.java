e79b4fbe3e9c6b62d6a20c251b8c71fa3e6a90e1
Francisco Perez <fran.14111996@gmail.com>
2019-09-17 12:25:38 +0100

    first commit

---
+++ src/main/java/dad/maven/hola/Main.java
@@ -0,0 +1,15 @@
+package dad.maven.hola;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String revez = StringUtils.reverse("Hola maven");
+		
+		System.out.println(revez);
+
+	}
+
+}
